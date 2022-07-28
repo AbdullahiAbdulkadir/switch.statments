c0a46616e4db466c6db39603d398acb44643a995
Abdullahiabdulkadir <Abdullahiabdulkadir0213999@gmail.com>
2022-07-28 13:12:26 +0100

    switch.statment

---
+++ script.js
@@ -0,0 +1,21 @@
+let roll = 11;
+
+switch (roll) {
+  case 1:
+    console.log("You rolled a 1");
+    break;
+  case 2:
+    console.log("You rolled a 2");
+    break;
+  case 3:
+    console.log("You rolled a 3");
+    break;
+  case 4:
+    console.log("You rolled a 4");
+    break;
+  case 5:
+    console.log("You rolled a 5");
+    break;
+    default:
+        console.log(roll, "The number is not possible");
+}
